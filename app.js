52055dcc0e78ae8a932e9305cc043b1db8834f6f
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2023-03-16 12:36:49 +0530

    add another section with animated video

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const text = intro.querySelector('h1');
 
 // END SECTION
 const section = document.querySelector('section');
+const video2 = section.querySelector('video');
 const end = section.querySelector('h1');
 
 // SCROLLMAGIC
@@ -32,6 +33,15 @@ let scene2 = new ScrollMagic.Scene({
   .setTween(textAnim)
   .addTo(controller);
 
+let scene3 = new ScrollMagic.Scene({
+  duration: 10000,
+  triggerElement: section,
+  triggerHook: 0
+})
+  .addIndicators()
+  .setPin(section)
+  .addTo(controller);
+
 // VIDEO ANIMATION
 let accelamount = 0.1; // easing the stop smoothly
 let scrollpos = 0;
@@ -46,3 +56,16 @@ setInterval(() => {
   video.currentTime = delay;
 }, 33.37783);
 // 33.3 = 1000 / 30 (100 / frame rate of the video)
+
+let accelamount2 = 0.1; // easing the stop smoothly
+let scrollpos2 = scrollpos;
+let delay2 = 0;
+
+scene3.on('update', e => {
+  scrollpos2 = e.scrollPos / 1000;
+})
+
+setInterval(() => {
+  delay2 += (scrollpos2 - delay2) * accelamount2;
+  video2.currentTime = delay2;
+}, 33.37783);
